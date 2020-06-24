d6031cd0ae986730c18dc845910a4e650a3f0218
Matthew Denaburg <EMAIL>
2020-06-23 20:07:54 -0400

    first pass at patient info form

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav(whichNav) {
+    document.getElementById(whichNav).style.width = "33%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav(whichNav) {
+    document.getElementById(whichNav).style.width = "0%";
+}
